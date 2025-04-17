6928a6b821cd4f0da1be6cd5f8a0b36c0c485bfb
ildar <ildarxan2008@gmail.com>
2025-04-17 19:28:30 +0500

    prosesing.py
    Я создал функцию, которая принимает 3 знвчения  str, bool, float и написал вызов функции чтоб она передавааоа эти параметры

---
+++ prosesing.py
@@ -0,0 +1,6 @@
+def processing(text: str, boolevoe: bool, plavat: float):
+    print(f'Stroka: {str}')
+    print(f'Bulevoe: {bool}')
+    print(f'Floatovoe: {float}')
+    
+print(processing("primer stroki", True, 224))
