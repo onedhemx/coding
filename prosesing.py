c5b64924f587c37af2d64344b77ced27f7259e79
ildar <ildarxan2008@gmail.com>
2025-04-17 20:22:18 +0500

    Update prosesing.py
    в коде добавилось то, что в цикле пропускается число 4 и цикл заканчивается на  9
     добовляется то что если число больше 50 то выводится сообщение о том что число больше 50
    и 2ой вызлв функции передающий другие значения

--- prosesing.py
+++ prosesing.py
@@ -1,5 +1,10 @@
 def processing(text: str, boolevoe: bool, plavat: 10.0):
     for num in range(int(plavat)):
+        if num == 4:
+            continue
+        if num == 9:
+            print(f"Квадрат числа {num} больше 50")
+            break
         print(f'chislo: {num}, kvadrat: {num **2}')
     print(f'Stroka: {str}')
     print(f'Bulevoe: {bool}')
@@ -7,4 +12,5 @@ def processing(text: str, boolevoe: bool, plavat: 10.0):
     
 processing("primer stroki", True, 10.0)
 
+processing('yooooo', False, 111)
 
