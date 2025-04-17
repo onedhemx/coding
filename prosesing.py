d6672267108e54e6b15653355f6aafc51d41035d
ildar <ildarxan2008@gmail.com>
2025-04-17 21:05:46 +0500

    Update prosesing.py
     В цикл добавьте вывод сообщения "Квадрат числа {number} больше или равен 20 и меньше или равен 50".
    
    2) В цикл добавьте вывод сообщения "Квадрат числа {number} меньше 20".
    
    3) В функцию добавьте проверку значения str на пустоту и если оно пусто вывавать исключение valueError.

--- prosesing.py
+++ prosesing.py
@@ -1,4 +1,6 @@
 def processing(text: str, boolevoe: bool, plavat: 10.0):
+    if text == "":
+            raise ValueError("Строка не должна быть пустой")
     for num in range(int(plavat)):
         if num == 4:
             continue
@@ -9,8 +11,12 @@ def processing(text: str, boolevoe: bool, plavat: 10.0):
     print(f'Stroka: {str}')
     print(f'Bulevoe: {bool}')
     print(f'Floatovoe: {float}')
+
+    try:
+        processing('', False, 111)
+    except ValueError as e:
+    print(f"ValueError: {e}")
     
 processing("primer stroki", True, 10.0)
 
-processing('yooooo', False, 111)
 
