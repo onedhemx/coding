7f4c74afd3677c6ba531fe8ffd1fd937f86815da
ildar <ildarxan2008@gmail.com>
2025-04-17 19:47:05 +0500

    Update prosesing.py
    код зименен теперь у него в plavat стоит 10.0 
    добавлен цикл который переберает все числа от 0 до 10.0 и выводит числа в квадрат

--- prosesing.py
+++ prosesing.py
@@ -1,6 +1,10 @@
-def processing(text: str, boolevoe: bool, plavat: float):
+def processing(text: str, boolevoe: bool, plavat: 10.0):
+    for num in range(int(plavat)):
+        print(f'chislo: {num}, kvadrat: {num **2}')
     print(f'Stroka: {str}')
     print(f'Bulevoe: {bool}')
     print(f'Floatovoe: {float}')
     
-print(processing("primer stroki", True, 224))
+processing("primer stroki", True, 10.0)
+
+
