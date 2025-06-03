80c7584fcb002b0446923e3313675e99c5499c1f
ildar <ildarxan2008@gmail.com>
2025-06-03 19:04:22 +0500

    создал класс энимал
    в коде создаетсяы класс и выводит имя возраст вид животноотого

---
+++ animals.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age 
+        
+animal = Animal('Kot', 'dog', 5)
+
+print(f'Имя: {self.name}, Вид: {self.species}, Возраст: {self.age}')
+
+
