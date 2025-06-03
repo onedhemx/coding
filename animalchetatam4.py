bfdab38f36e4c309a198a3dde11f0a4c8a2ec09f
ildar <ildarxan2008@gmail.com>
2025-06-03 19:40:51 +0500

    Create animalchetatam4.py

---
+++ animalchetatam4.py
@@ -0,0 +1,33 @@
+class Animal:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age 
+        
+    def display_info(self):
+        print(f'Имя: {animal.name}, Вид: {animal.species}, Возраст: {animal.age}')
+        
+animal = Animal('собвака', 'кошка', 1)
+animal.display_info()
+
+
+class Shelter:
+    def __init__(self):
+        self.animals = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+        
+    def show_animals(self):
+        if self.animals == 0:
+            print( "Приют пуст")
+        else:
+            animal.display_info()
+            
+            
+shshsh = Shelter()
+animal = Animal("Мур", "собака", 2)
+shshsh.add_animal(animal)
+
+for a in shshsh.animals:
+    a.display_info()
