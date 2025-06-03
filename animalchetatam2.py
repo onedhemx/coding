b094d46c45768f0773592b4ac51f8128eaa75c62
ildar <ildarxan2008@gmail.com>
2025-06-03 19:20:12 +0500

    создал пустой список

---
+++ animalchetatam2.py
@@ -0,0 +1,17 @@
+
+class Animal:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age 
+        
+    def display_info(self):
+        print(f'Имя: {animal.name}, Вид: {animal.species}, Возраст: {animal.age}')
+        
+animal = Animal('собвака', 'кошка', 1)
+animal.display_info()
+
+
+class Shelter:
+    def __init__(self):
+        self.animal = []
