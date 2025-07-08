9c4ebdaad5b42f4bd648ce6a18570bb00c907cee
ildar <ildarxan2008@gmail.com>
2025-07-08 20:06:12 +0500

    Update test_unit.py

--- test_unit.py
+++ test_unit.py
@@ -1,6 +1,8 @@
+import unittest
+from calculations import Vichisl
 
 class Vichisl:
-    def __init__(self):
+    def factorial(self):
         
         n = int(input())
 
@@ -12,7 +14,7 @@ class Vichisl:
         print(factorial)
         
         
-    def __init__(self):
+    def stepen(self, a , b):
         a = float(input('введите основание '))
         b = float(input('введите показ степени'))
         
@@ -20,24 +22,29 @@ class Vichisl:
         
         print(F'{a} в степени {b} равно {res}')
         
-    def __init__(self):
+    def c_calculation(self, Q, m, t):
         
         Q = int(input('введите Q'))
         m = int(input('введите m'))
         t = int(input('введите t'))
         c = Q/(m * t)
         print(c)
-
-
-
-
-
-
-
-
-
-
-
-
         
+        
+class TestCalculations(unittest.TestCase):
+    def setUp(self):
+        self.calc = Vichisl()
+
+    def test_factorial(self):
+        self.assertEqual(self.calc.factorial(7), 5040)
+        self.assertEqual(self.calc.factorial(4), 24)
+
+    def test_power(self):
+        self.assertAlmostEqual(self.calc.power(4, 3), 64)
+        self.assertAlmostEqual(self.calc.power(2, 8), 256)
+
+    def test_c_calculation(self):
+        self.assertAlmostEqual(self.calc.c_calculation(10, 2, 5), 1.0)
+        self.assertAlmostEqual(self.calc.c_calculation(20, 4, 2), 2.5)
+
 a = Vichisl()
