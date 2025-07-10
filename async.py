a75bad35aee7d035c51d43503665ef9b836c052c
ildar <ildarxan2008@gmail.com>
2025-07-10 20:04:58 +0500

    Update async.py

--- async.py
+++ async.py
@@ -15,3 +15,24 @@ words = [
 for word in words:
     print(word)
     time.sleep(1)
+
+
+
+
+import asyncio
+
+async def say_hello(name, delay):
+    await asyncio.sleep(delay)
+    print(f'{name} after {delay} seconds!')
+
+async def main():
+    await asyncio.gather(
+        say_hello('прив', 2),
+        say_hello('пивет', 3),
+        say_hello('пр', 0.5),
+        say_hello('приверттттт', 8),
+        say_hello('ппппппппппппррррррррррррррииииииииииииииииииииииввввввввввввввввввеееееееееет', 25)
+    )
+    print('end')
+
+asyncio.run(main())
