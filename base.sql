e46579c4bb7b740bef30ebdb8eb2d81997c396fd
ildar <ildarxan2008@gmail.com>
2025-07-17 19:14:15 +0300

    Update base.sql

--- base.sql
+++ base.sql
@@ -19,25 +19,49 @@ CREATE TABLE books (
     year INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (author_id) REFERENCES authors(id),
-    FOREIGN KEY (publisher_id) REFERENCES publishers(id)
+    FOREIGN KEY (publisher_id) REFERENCES publishers(id) 
 );
 
+-- Вставка авторов
+INSERT INTO authors(firstname, lastname) VALUES ('Уильям', 'Шекспир');
+INSERT INTO authors(firstname, lastname) VALUES ('Виктор', 'Пелевин');
+INSERT INTO authors(firstname, lastname) VALUES ('Чарльз', 'Диккенс');
+INSERT INTO authors(firstname, lastname) VALUES ('Эдгар', 'Аллан По');
 
-INSERT INTO authors(firstname, lastname) VALUES ("Уильям", "Шекспир");
-INSERT INTO authors(firstname, lastname) VALUES ("Виктор", "Пелевин");
-INSERT INTO authors(firstname, lastname) VALUES ("Чарльз", "Диккенс");
+-- Вставка издателей
+INSERT INTO publishers(name) VALUES ('Эксмо');
+INSERT INTO publishers(name) VALUES ('Азбука-Аттикус');
 
-SELECT * FROM authors;
-
-INSERT INTO publishers(name) VALUES ("Эксмо");
-INSERT INTO publishers(name) VALUES ("Азбука-Аттикус");
-
-SELECT * FROM publishers;
-
+-- Вставка книг (исправленная версия)
 INSERT INTO books (title, author_id, publisher_id, year) VALUES
 ('Гамлет', 1, 1, 1603),
 ('Ромео и Джульетта', 1, 1, 1597),
 ('Generation П', 2, 2, 1999),
-('Оливер Твист', 3, 2, 1838);
+('Оливер Твист', 3, 2, 1838),
+('Лигейя', 4, 2, 1838);  -- Исправлены ID автора и издателя
 
+-- Просмотр всех книг
 SELECT * FROM books;
+
+-- Поиск книг 1838 года
+SELECT b.title AS 'Название книги',
+       CONCAT(a.firstname, ' ', a.lastname) AS 'Автор',
+       p.name AS 'Издательство',
+       b.year AS 'Год издания'
+FROM books b
+JOIN authors a ON b.author_id = a.id
+JOIN publishers p ON b.publisher_id = p.id
+WHERE b.year = 1838;
+
+-- Обновляем год издания для книги "Гамлет"
+UPDATE books
+SET year = 1601
+WHERE title = 'Гамлет' AND author_id = 1;  -- Уильям Шекспир
+
+-- Проверяем изменение
+SELECT b.title, 
+       CONCAT(a.firstname, ' ', a.lastname) AS author,
+       b.year AS new_year
+FROM books b
+JOIN authors a ON b.author_id = a.id
+WHERE b.title = 'Гамлет';
